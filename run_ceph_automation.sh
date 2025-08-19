11f3f0866a2cddf02eb0e25438d56bcb3bba194e
Yarin Mozes <yarinmozes@gmail.com>
2025-08-19 15:21:54 +0300

    feat: Add Ansible playbook for Ceph compilation and verification
    This change introduces a new Ansible playbook (ceph_playbook.yml)
    that automates the full lifecycle of compiling Ceph from source and
    verifying its RGW REST API.
    
    The playbook is idempotent and split into 'compile' and 'verify' tags.
    It has been integrated into the main bootstrap script via a new
    wrapper script (run_ceph_automation.sh) to be executed by the CI.
    A README file has been added to explain its usage.
    
    This fulfills the requirements for the automation part of the task.
    
    Trello Card:https://trello.com/c/DsfezhH5/48-devbluestore-compile-and-verify-bluestore-restapi-usage
    
    Change-Id: I5118cd4727c2096806ebff46159aecde0402b499

---
+++ run_ceph_automation.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+
+echo "--- Starting Ceph Compilation ---"
+ansible-playbook ceph_playbook.yml -i hosts --tags compile
+
+# echo "--- Starting Ceph Verification ---"
+# ansible-playbook ceph_playbook.yml -i hosts --tags verify
